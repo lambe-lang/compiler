542d2d50b250e56a68765596cff600b43d5d9d0a
d-plaindoux <d.plaindoux@free.fr>
2020-11-23 08:52:53 +0100

    TC in progress: refactoring

--- test/checker/main_test.ml
+++ test/checker/main_test.ml
@@ -1,3 +1,3 @@
 let () =
   let open Alcotest in
-  run "checker" [ Freevariables_test.test_cases; TypeChecker_test.test_cases ]
+  run "checker" [ Variables_test.test_cases; Type_check_test.test_cases ]
