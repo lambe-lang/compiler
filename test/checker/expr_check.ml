dae55b2244372467d86be56b5eaa5f20d7b8cf16
d-plaindoux <d.plaindoux@free.fr>
2021-03-22 12:23:54 +0100

    Expressions typecheker in progress

--- test/checker/expr_check.ml
+++ test/checker/expr_check.ml
@@ -64,17 +64,88 @@ let test_case_005 () =
     "should reject r:data d (v:int) |- r :? data d (v:int) (w:int)" expected
     computed
 
+let test_case_006 () =
+  let expected = true
+  and computed, _ =
+    H.k_set [ "int", K.star ] + H.s_set [ ("r", T.(data "r" [ "v", v "int" ])) ]
+    |- (use (v "r") (v "v") <:?> T.v "int") Variables.create
+  in
+  Alcotest.(check bool)
+    "should accept r:data d (v:int) |- r.v :? data d (v:int)" expected computed
+
+let test_case_007 () =
+  let expected = false
+  and computed, _ =
+    H.k_set [ "int", K.star ] + H.s_set [ ("r", T.(data "r" [ "v", v "int" ])) ]
+    |- (use (v "r") (v "w") <:?> T.v "int") Variables.create
+  in
+  Alcotest.(check bool)
+    "should accept r:data d (v:int) |- r.w :? int" expected computed
+
+let test_case_008 () =
+  let expected = true
+  and computed, _ =
+    H.k_set [ "int", K.star ]
+    + H.s_set [ ("r", T.(trait [] [] [ "v", v "int" ] [])) ]
+    |- (use (v "r") (v "v") <:?> T.v "int") Variables.create
+  in
+  Alcotest.(check bool)
+    "should accept r:trait { sig v:int } |- r.v :? int" expected computed
+
+let test_case_009 () =
+  let expected = false
+  and computed, _ =
+    H.k_set [ "int", K.star ]
+    + H.s_set [ ("r", T.(trait [] [] [ "v", v "int" ] [])) ]
+    |- (use (v "r") (v "w") <:?> T.v "int") Variables.create
+  in
+  Alcotest.(check bool)
+    "should reject r:trait { sig v:int } |- r.w :? int" expected computed
+
+let test_case_010 () =
+  let expected = true
+  and computed, _ =
+    H.k_set [ "int", K.star ]
+    + H.s_set [ ("r", T.(trait [] [] [ "v", data "r" [ "w", v "int" ] ] [])) ]
+    |- (use (use (v "r") (v "v")) (v "w") <:?> T.v "int") Variables.create
+  in
+  Alcotest.(check bool)
+    "should accept r:trait { sig v:data r (w:int) } |- r.v :? int" expected
+    computed
+
+let test_case_011 () =
+  let expected = true
+  and computed, _ =
+    H.k_set [ "int", K.star ]
+    + H.s_set [ ("r", T.(data "r" [ "v", trait [] [] [ "w", v "int" ] [] ])) ]
+    |- (use (use (v "r") (v "v")) (v "w") <:?> T.v "int") Variables.create
+  in
+  Alcotest.(check bool)
+    "should accept r:data r (v:trait { sig w:int }) |- r.v.w :? int" expected
+    computed
+
 let test_cases =
   let open Alcotest in
   ( "Expression check"
   , [
       test_case "Accept a :? int" `Quick test_case_000
     ; test_case "Accept { x -> x } :? int -> int" `Quick test_case_001
-    ; test_case "Accept {> self } :? int |@> int" `Quick test_case_002
+    ; test_case "Accept { x -> x } :? int |@> int" `Quick test_case_002
     ; test_case "Accept r:data d (v:int) |- r :? data d (v:int)" `Quick
         test_case_003
     ; test_case "Accept r:data d (v:int) (w:int) |- r :? data d (v:int)" `Quick
         test_case_004
     ; test_case "Reject r:data d (v:int) |- r :? data d (v:int) (w:int)" `Quick
         test_case_005
+    ; test_case "Accept r:data d (v:int) |- r.v :? data d (v:int)" `Quick
+        test_case_006
+    ; test_case "Reject r:data d (v:int) |- r.w :? int" `Quick test_case_007
+    ; test_case "Accept r:trait { sig v:int } |- r.v :? int" `Quick
+        test_case_008
+    ; test_case "Reject r:trait { sig v:int } |- r.w :? int" `Quick
+        test_case_009
+    ; test_case "Accept r:trait { sig v:data r (w:int) ] } |- r.v.w :? int"
+        `Quick test_case_010
+    ; test_case "Accept r:data r (v:trait { sig w:int }) |- r.v.w :? int" `Quick
+        test_case_011
     ] )
